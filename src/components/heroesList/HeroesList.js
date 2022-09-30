8bd70c8a0074f63f6da4d771093966c3406b1da9
Vlad Galayko <lorddarkvlad@gmail.com>
2022-09-30 22:36:54 +0300

    redux

---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,77 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useEffect, useCallback} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './heroesList.scss'
+
+// Задача для этого компонента:
+// При клике на "крестик" идет удаление персонажа из общего состояния
+// Усложненная задача:
+// Удаление идет и с json файла при помощи метода DELETE
+
+const HeroesList = () => {
+    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(heroesFetching());
+        request("http://localhost:3001/heroes")
+            .then(data => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()))
+
+        // eslint-disable-next-line
+    }, []);
+
+    const onDeleteHero = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+            .then(data => console.log(data, 'Deleted'))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err));
+            // eslint-disable-next-line
+    }, [request])
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return (
+                <CSSTransition
+                timeout={0}
+                classNames="hero">
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            )
+        }
+
+        return arr.map(({id, ...props}) => {
+            return (
+                <CSSTransition
+                    key={id}
+                    timeout={500}
+                    classNames="hero">
+                    <HeroesListItem key={id} {...props} onDeleteHero={() => onDeleteHero(id)}/>
+                </CSSTransition>
+            )
+        })
+    }
+
+
+    const elements = renderHeroesList(heroes);
+    return (
+        <TransitionGroup component="ul">
+                {elements}
+        </TransitionGroup>
+        
+    )
+}
+
+export default HeroesList;
