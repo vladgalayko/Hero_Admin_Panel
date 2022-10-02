d9de9910b5db70c77c9cf0849aa5ef9b74222a7f
Vlad Galayko <lorddarkvlad@gmail.com>
2022-10-02 18:48:52 +0300

    add middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,35 @@
-import { createStore, combineReducers, compose} from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import heroesReducer from '../reducers/heroes';
 import filtersReducer from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store
-}
+    return next(action)
+};
+
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action)
+//     }
+//     return store
+// }
 
 const store = createStore( 
                 combineReducers({heroesReducer, filtersReducer}), 
                 compose(
-                    enhancer,
+                    applyMiddleware(stringMiddleware),
                     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
                 ));
 
